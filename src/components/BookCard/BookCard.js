06ffb57104192ed471ef5f74361057a39b27fa23
claire greenwood <cg.clairegreenwood@gmail.com>
2023-03-24 16:13:36 +0000

    bookcards edited and mylibrary page code edited to ensure responsive design

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+
 import "./BookCard.css";
 
 function BookCard(props) {
 
-    const { id, image, title, author, removeBook } = props;
+    const { id, image, title, author, averageRating } = props;
    
-    const handleRemove = () => {
-        removeBook(id);
-    }
 
     return (
-            <Card className="book" key={id}>
-            <div className="img-container">
-            <Card.Img className="image" variant="top" alt="book image" src={image}/>
-            </div>
-            <Card.Body>
-            <Card.Title className="title">{title}</Card.Title>
-            <Card.Subtitle className="author">Author: {author}</Card.Subtitle>
-            <Button className="removeButton" variant="danger" onClick={handleRemove}>Remove</Button>
-            </Card.Body>
-            </Card>
+    <div className="card" key={id}>
+       <div className="img-container">
+           <img alt="book cover" src={image} />
+       </div>
+       <div className="content">
+            <ul>
+                <li className="title">
+                  <strong>Title:</strong> {title}
+                </li>
+                <li className="author">
+                  <strong>Author:</strong> {author}
+                </li>
+                <li className="averageRating">
+                  <strong>Average rating:</strong> {averageRating}
+                </li>
+            </ul>
+        </div>
+    </div>
+            // <Card className="book" key={id}>
+            // <div className="img-container">
+            // <Card.Img className="image" variant="top" alt="book image" src={image}/>
+            // </div>
+            // <Card.Body>
+            // <Card.Title className="title">{title}</Card.Title>
+            // <Card.Subtitle className="author">Author: {author}</Card.Subtitle>
+            // <Button className="removeButton" variant="danger" onClick={handleRemove}>Remove</Button>
+            // </Card.Body>
+            // </Card>
     );
   }
   
--- src/components/pages/MyLibrary/MyLibrary.js
+++ src/components/pages/MyLibrary/MyLibrary.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./MyLibrary.css"; 
 import BookCard from "../../BookCard/BookCard";
@@ -9,73 +10,102 @@ function MyLibrary() {
 
 
 return (
-    <div>
-      <div className="container">
-        <h2>My Library</h2>
-        <div className="row current justify-content-center">
-            <h3>Want To Read</h3>
-            <div className="col">
-            <BookCard
-            id={library[0].id}
-            title={library[0].title}
-            image={library[0].image}
-            author={library[0].author}>
-            </BookCard>
-            </div>
-            <div className="col">
-            <BookCard
-            id={library[1].id}
-            title={library[1].title}
-            image={library[1].image}
-            author={library[1].author}>
-            </BookCard>
-            </div>
-            {/* {currentlyReading.map((book) => (
+    <div className="bg myLib">
+        <h1>My Library</h1>
+    <Container>
+        <Row className="wantRead mb-3">
+        <h3>Want To Read</h3>
                 <Col>
-                <BookCard book={book} />
+                <BookCard
+                id={library[0].id}
+                title={library[0].title}
+                image={library[0].image}
+                author={library[0].author}
+                averageRating={library[0].averageRating}>
+                
+                </BookCard>
                 </Col>
-            ))} */}
-        </div>
-        <div className="row favourites justify-content-center">
+                <Col>
+                <BookCard
+                id={library[1].id}
+                title={library[1].title}
+                image={library[1].image}
+                author={library[1].author}
+                 averageRating={library[0].averageRating}>
+                </BookCard>
+                </Col>
+                <Col>
+                <BookCard
+                id={library[2].id}
+                title={library[2].title}
+                image={library[2].image}
+                author={library[2].author}
+                 averageRating={library[0].averageRating}>
+                </BookCard>
+                </Col>
+            </Row>
+            <Row className="current mb-3">
             <h3>Currently Reading</h3>
-            <div className="col">
+            <Col>
             <BookCard
             id={library[2].id}
             title={library[2].title}
             image={library[2].image}
-            author={library[2].author}>
+            author={library[2].author}
+             averageRating={library[0].averageRating}>
             </BookCard>
-            </div>
-                {/* {favourites.map((book) => (
-                <Col>
-                <BookCard book={book} />
-                </Col> 
-            ))}  */}
-        </div>
-        <div className="row previous justify-content-center">
-            <h3>Previously Read</h3>
-            <div className="col">
+            </Col>
+            <Col>
             <BookCard
             id={library[3].id}
             title={library[3].title}
             image={library[3].image}
-            author={library[3].author}>
+            author={library[3].author}
+             averageRating={library[0].averageRating}>
             </BookCard>
+            </Col>
+            <Col>
+            <BookCard
+            id={library[3].id}
+            title={library[3].title}
+            image={library[3].image}
+            author={library[3].author}
+             averageRating={library[0].averageRating}>
+            </BookCard>
+            </Col>
+            </Row>
+            <Row className="previous mb-3">
+            <h3>Previously Read</h3>
+            <Col>
+            <BookCard
+            id={library[3].id}
+            title={library[3].title}
+            image={library[3].image}
+            author={library[3].author}
+             averageRating={library[0].averageRating}>
+            </BookCard>
+            </Col>
+            <Col>
             <BookCard
             id={library[4].id}
             title={library[4].title}
             image={library[4].image}
-            author={library[4].author}>
+            author={library[4].author}
+            averageRating={library[0].averageRating}>
             </BookCard>
-            {/* {previouslyRead.map((book) => (
-                <Col>
-                <BookCard className="bookCard" book={book} />
-                </Col>
-            ))} */}
-            </div>
-        </div>
+            </Col>
+            <Col>
+            <BookCard
+            id={library[0].id}
+            title={library[0].title}
+            image={library[0].image}
+            author={library[0].author}
+            averageRating={library[0].averageRating}>
+            </BookCard>
+            </Col>
+            </Row>
+      </Container>
       </div>
-    </div>
   );
 }
 
