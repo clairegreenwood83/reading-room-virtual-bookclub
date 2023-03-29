d93ef5e52f55ddb50692448b78d8f8ce5169e960
Heil Park <heilpark@me.com>
2023-03-29 20:36:52 +0100

    fixed mylibrary functionality

--- src/components/pages/MyLibrary/MyLibrary.js
+++ src/components/pages/MyLibrary/MyLibrary.js
@@ -6,69 +6,22 @@ import BookCard from "../../BookCard/BookCard";
 
 function MyLibrary() {
 
-//const library = JSON.parse(localStorage.getItem('myLibrary')) | [];
-//console.log(library);
-
-const [books, setBooks] = useState(JSON.parse(localStorage.getItem("myLibrary")));
+const [books, setBooks] = useState(JSON.parse(localStorage?.getItem("myLibrary")));
 const [currentlyReading, setCurrentlyReading] = useState(JSON.parse(localStorage.getItem("currentlyReading")) || []);
 const [previouslyRead, setPreviouslyRead] = useState(JSON.parse(localStorage.getItem("previouslyRead")) || []);
 
 useEffect(() => {
-  localStorage.setItem("myLibrary", JSON.stringify(books));
-  localStorage.setItem("currentlyReading", JSON.stringify(currentlyReading));
-  localStorage.setItem("previouslyRead", JSON.stringify(previouslyRead));
-}, [books, currentlyReading, previouslyRead]);
+  const interval = setInterval(() => {
+    setBooks(JSON.parse(localStorage.getItem("myLibrary")));
+    setCurrentlyReading(JSON.parse(localStorage.getItem("currentlyReading")));
+    setPreviouslyRead(JSON.parse(localStorage.getItem("previouslyRead")));
+  }, 500);
 
-
-const removeBook = (id) => {
-    const newBookList = books.filter((library) => library.id !== id);
-    setBooks(newBookList);
-    setCurrentlyReading(currentlyReading.filter((book) => book.id !== id));
-    setPreviouslyRead(previouslyRead.filter((book) => book.id !== id));
-    localStorage.setItem("myLibrary", JSON.stringify(newBookList));
-    localStorage.setItem("currentlyReading", JSON.stringify(currentlyReading));
-    localStorage.setItem("previouslyRead", JSON.stringify(previouslyRead));
-    };
-
-const moveBook = (id, destination) => {
-        const book = books.find((book) => book.id === id);
-        const newBookList = books.filter((book) => book.id !== id);
-
-        const removeFromRow = (row) =>
-          row.filter((book) => book.id !== id);
-        
-        let updatedCurrentlyReading = [...currentlyReading];
-        let updatedPreviouslyRead = [...previouslyRead];
-
-          if (destination === "currentlyReading") {
-            setCurrentlyReading([...currentlyReading, book]);
-            updatedPreviouslyRead = removeFromRow(previouslyRead);
-            setPreviouslyRead(updatedPreviouslyRead);
-            localStorage.setItem("currentlyReading", JSON.stringify([...currentlyReading, book]));
-            localStorage.setItem("previouslyRead", JSON.stringify(updatedPreviouslyRead));
-        } else if (destination === "previouslyRead") {
-            setPreviouslyRead([...previouslyRead, book]);
-            updatedCurrentlyReading = removeFromRow(currentlyReading);
-            setCurrentlyReading(updatedCurrentlyReading);
-            localStorage.setItem("previouslyRead", JSON.stringify([...previouslyRead, book]));
-            localStorage.setItem("currentlyReading", JSON.stringify(updatedCurrentlyReading));
-        } else if (destination === "favourites") {
-          const newCurrentlyReading = currentlyReading.filter((book) => book.id !== id);
-          const newPreviouslyRead = previouslyRead.filter((book) => book.id !== id);  
-            setCurrentlyReading(updatedCurrentlyReading);
-            setPreviouslyRead(updatedPreviouslyRead);
-            setBooks([...books, book]); // add book back to favourites row
-            localStorage.setItem("myLibrary", JSON.stringify([...books, book])); // update localStorage for myLibrary
-            localStorage.setItem("currentlyReading", JSON.stringify(newCurrentlyReading));
-            localStorage.setItem("previouslyRead", JSON.stringify(newPreviouslyRead));
-          }
-        setBooks(newBookList);
-        // save updated arrays to local storage
-
-        localStorage.setItem("myLibrary", JSON.stringify(newBookList));
-
-    };
-
+  return () => {
+    console.log(`clearing interval`);
+    clearInterval(interval);
+  }
+}, []);
 
 return (
     <div className="bg myLib">
@@ -76,48 +29,42 @@ return (
     <Container>
         <Row className="favourites mb-3" style={{ overflowX: "auto" }}>
             <h3>Favourites</h3>
-            {books.map(book => (
-                <Col key={book.id}>
+            {books?.map(book => (
+                <Col key={book?.id}>
                 <BookCard
-                moveBook={moveBook}
-                removeBook={removeBook}
-                id={book.id}
-                title={book.title}
-                image={book.imageLinks.thumbnail}
-                author={book.authors[0]}
-                averageRating={book.averageRating}>
-                </BookCard>
+                  id={book?.id}
+                  currStatus={"myLibrary"}
+                  title={book?.title}
+                  image={book?.imageLinks.thumbnail}
+                  author={book?.authors[0]}
+                />
                 </Col>
             ))}
         </Row>
         <Row className="current mb-3" style={{ overflowX: "auto" }}>
             <h3>Currently Reading</h3>
-            {currentlyReading.map((book) => (
-            <Col key={book.id}>
+            {currentlyReading?.map((book) => (
+            <Col key={book?.id}>
               <BookCard
-                removeBook={removeBook}
-                moveBook={moveBook}
-                id={book.id}
-                title={book.title}
-                image={book.image}
-                author={book.author}
-                averageRating={book.averageRating}
+                id={book?.id}
+                currStatus={"currentlyReading"}
+                title={book?.title}
+                image={book?.imageLinks.thumbnail}
+                author={book?.authors[0]}
               />
             </Col>
            ))}
         </Row>
         <Row className="previous mb-3" style={{ overflowX: "auto" }}>
             <h3>Previously Read</h3>
-           {previouslyRead.map((book) => (
-            <Col key={book.id}>
+           {previouslyRead?.map((book) => (
+            <Col key={book?.id}>
               <BookCard
-                removeBook={removeBook}
-                moveBook={moveBook}
-                id={book.id}
-                title={book.title}
-                image={book.image}
-                author={book.author}
-                averageRating={book.averageRating}
+                id={book?.id}
+                currStatus={"previouslyRead"}
+                title={book?.title}
+                image={book?.imageLinks.thumbnail}
+                author={book?.authors[0]}
               />
             </Col>
           ))}
