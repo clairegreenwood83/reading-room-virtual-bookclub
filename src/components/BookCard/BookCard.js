1e9c146e2924fddbb4d1b97df8f9480f88415a29
Vali Manea <118940094+valimanea@users.noreply.github.com>
2023-03-30 00:51:06 +0100

    added carousel for my library and styled the cards

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React from "react";
 import "./BookCard.css";
 
-function BookCard(props) { 
-
+function BookCard(props) {
   if (!props) {
     return;
   }
@@ -10,20 +9,20 @@ function BookCard(props) {
   const { id, currStatus, image, title, author } = props;
 
   const handleMovebook = (event) => {
-    let destin =  event.target.value;
+    let destin = event.target.value;
 
     if (destin === currStatus) {
       console.log("Already in category!");
       return;
     }
 
-    let obooks =  JSON.parse(localStorage.getItem(currStatus));
-    let nbooks =  JSON.parse(localStorage.getItem(destin));
+    let obooks = JSON.parse(localStorage.getItem(currStatus));
+    let nbooks = JSON.parse(localStorage.getItem(destin));
 
-    if (!nbooks ) {
+    if (!nbooks) {
       nbooks = [];
     } else if (!obooks) {
-      obooks = []; 
+      obooks = [];
     }
 
     const movingB = obooks.find((book) => book?.title === title);
@@ -31,45 +30,45 @@ function BookCard(props) {
 
     localStorage.setItem(currStatus, JSON.stringify(newoBookList));
     localStorage.setItem(destin, JSON.stringify([...nbooks, movingB]));
-  }
+  };
 
   const removeBook = () => {
-    let books =  JSON.parse(localStorage.getItem(currStatus));
-    const newBookList = books.filter((book) => book?.title !== title); 
+    let books = JSON.parse(localStorage.getItem(currStatus));
+    const newBookList = books.filter((book) => book?.title !== title);
     localStorage.setItem(currStatus, JSON.stringify(newBookList));
   };
 
-    return (
-  <div>
-    <div className="card" key={id}>
-       <div className="img-container">
-           <img alt="book cover" src={image} />
-       </div>
-       <div className="content">
-            <ul>
-                <li className="title">
-                  <strong>Title:</strong> {title}
-                </li>
-                <li className="author">
-                  <strong>Author:</strong> {author}
-                </li>
-            </ul>
+  return (
+    <div>
+      <div className="mycard" key={id}>
+        <img alt="book cover" src={image} />
+        <div className="bottom">
+          <h3 className="title">
+            <em>{title}</em>
+          </h3>
+          <h3 className="data">
+            <strong>Author: </strong>
+            <em>{author} </em>
+          </h3>
+          <div className="movement">
+            <div className="dropdown">
+              <select onChange={handleMovebook}>
+                <option value="">Move to:</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="previouslyRead">Previously Read</option>
+                <option value="myLibrary">Favourites</option>
+              </select>
+            </div>
+            <div className="removeBtn">
+              <button onClick={removeBook} className="remove">
+                Remove
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="dropdown">
-          <select onChange={handleMovebook}>
-                        <option value="">Move to:</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="previouslyRead">Previously Read</option>
-                        <option value="myLibrary">Favourites</option>
-                    </select>
-      </div>
-      <div className="removeBtn">
-        <button onClick={removeBook} className="remove">Remove</button>
-      </div>
-  </div>
+    </div>
+  );
+}
 
-    );
-  }
-  
-  export default BookCard;
+export default BookCard;
